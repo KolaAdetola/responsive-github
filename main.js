2566b2bf3be837e8f2f6dde527c5e0f652251833
kolawoleadetola630 <kolawoleadetola630@gmail.com>
2024-07-24 23:57:11 +0000

    kll

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // Add JS here
+let navbar=document.querySelector("#kola");
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2244343645.
+addEventListener("click",()=>{
+    navbar.style.backgroundColor="pink";
+})
+console.log(navbar);
+console.log("hello");
