de96baeb5be01e31669ce06e17dd15c023797715
kolawoleadetola630 <kolawoleadetola630@gmail.com>
2024-08-25 21:17:10 +0000

    removed nonsense

--- main.js
+++
@@ -1,7 +0,0 @@
-console.log("hello");
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:1375055149.
-let hamburger= document.querySelector("#hamburger");
-console.log(hamburger);
-addEventListener("click", function(){
-    hamburger.style.display = "none";
-});
